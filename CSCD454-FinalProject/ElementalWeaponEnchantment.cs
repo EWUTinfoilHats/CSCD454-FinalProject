e29fcd1229a9392496f7323574a1d20a52057f57
Stefan Bostain <zilulil@gmail.com>
2014-08-09 14:29:30 -0700

    Added Weapon Enchantments Masterwork, Basic enchantments, elemental enchantments, elemental burst

---
+++ CSCD454-FinalProject/ElementalWeaponEnchantment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSCD454_FinalProject.Dice;
+
+namespace CSCD454_FinalProject.Items
+{
+    public class ElementalWeaponEnchantment : WeaponEnchantment
+    {
+        public ElementalWeaponEnchantment(Weapon baseWeapon) : base(baseWeapon)
+        {
+
+        }
+
+        public override int GetDamageRoll(int strength, bool isOffhand)
+        {
+            return DamageHelper(strength, isOffhand) + D6.GetInstance().Roll();
+        }
+
+        public override int GetCriticalDamageRoll(int strength, bool isOffhand)
+        {
+            return base.GetCriticalDamageRoll(strength, isOffhand) + D6.GetInstance().Roll();
+        }
+    }
+}
---
+++ CSCD454-FinalProject/MasterworkWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCD454_FinalProject.Items
+{
+    public class MasterworkWeapon : WeaponEnchantment
+    {
+        public MasterworkWeapon(Weapon baseWep) : base(baseWep)
+        {
+            enchantedWeapon = baseWep.GetBaseWeapon();
+        }
+
+        public override int AttackMod
+        {
+            get
+            {
+                return 1;
+            }
+        }
+    }
+}
