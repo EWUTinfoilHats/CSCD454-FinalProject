37ba170d12d83e1e6204a794d01f377b11bbcf2a
ScubaSteve5689 <sberg1989@gmail.com>
2014-08-13 20:20:25 -0700

    Generates an array of rooms / tiles
    creates a random sized 2D array of rooms. Based on toggle determines if
    every tile has monster or only 30% do. Toggle default set to 30% must be
    set true before dungeon creation for 100% encounters.

--- CSCD454-FinalProject/Maze/DungeonMaze.cs
+++ CSCD454-FinalProject/Maze/DungeonMaze.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSCD454_FinalProject.Dice;
+using CSCD454_FinalProject.Combat;
 
 namespace CSCD454_FinalProject.Maze
 {
@@ -14,10 +16,76 @@ namespace CSCD454_FinalProject.Maze
             protected set;
         }
 
+        public int rows{ get; protected set; }
+
+        public int cols { get; protected set; }
+
         protected void generate()
         {
-            //procedurally gen maze
-            throw new NotImplementedException();
+            makeMaze();
+            fillMaze();
+        }
+
+        private Boolean addEncounter()
+        {
+            int roll = D100.GetInstance().Roll();
+            if (roll >= 70)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private Room addRoom()
+        {
+            if (spawn)
+            {
+                return new Room(true);
+            }
+            else
+            {
+                if (addEncounter())
+                {
+                    return new Room(true);
+                }
+
+                return new Room(false);
+            }
+        }
+
+        private void fillMaze()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    maze[i, j] = addRoom();
+                    
+                }
+            }
+        }
+
+        private void makeMaze()
+        {
+            Random rand = new Random();
+            rows = rand.Next(5, 21);
+            cols = rand.Next(5, 21);
+            maze = new Room[rows, cols];
+        }
+
+//Cheat flags=====================================
+        private Boolean _spawn = false;
+        public Boolean spawn
+        {
+            get
+            {
+                return _spawn;
+            }
+            set
+            {
+                _spawn = value;
+            }
         }
     }
 }
--- CSCD454-FinalProject/Maze/Room.cs
+++ CSCD454-FinalProject/Maze/Room.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSCD454_FinalProject.Combat;
+using CSCD454_FinalProject.Entitys;
 
 namespace CSCD454_FinalProject.Maze
 {
@@ -11,17 +12,37 @@ namespace CSCD454_FinalProject.Maze
     {
         public Encounter encounter
         {
-            get;
+            get
+            {
+                return encounter;
+            }
             protected set
             {
                 encounter = value;
             }
         }
 
+        public Room(Boolean encounter)
+        {
+            this.hasEncounter = encounter;
+        }
+
         public Boolean hasEncounter
         {
             get;
             protected set;
+        }
+
+        public void onEnter(IList<Player> party)
+        {
+            if (this.hasEncounter)
+            {
+                //trigger combat
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
