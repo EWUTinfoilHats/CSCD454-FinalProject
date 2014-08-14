b3e50897750919cf2f8831edb1fae8377015de32
ScubaSteve5689 <sberg1989@gmail.com>
2014-08-13 19:30:19 -0700

    Base hooks for maze and rooms
    skeleton stuff

--- CSCD454-FinalProject/EncounterGen/EncounterGen.cs
+++ CSCD454-FinalProject/EncounterGen/EncounterGen.cs
@@ -12,10 +12,10 @@ namespace CSCD454_FinalProject.EncounterGeneration
 {
     class EncounterGen
     {
-        private static int[] rollTable = new int[] { 0,0,0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,4,4,5,5,5,5,5,5,6,6,7,7,8,8,8,8,8,8,8,8,9,9,9,9,10,10,10,10,10,10,11,11,12,12,12,12,13,13,13,13,14,14,14,14,15,15,16,16,16,16,16,16,17,17,17,17,17,17,18,18,18,18,19,19,19,19,20,20,21,21,22,22,23,23,23,23 };
-        private static int[] ChallengeRating = new int[] { 1,1,1,1,1,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4 };
-        private static int[] EnemyNumbers = new int[] { 6,6,6,4,1,6,1,1,12,4,4,1,1,1,1,1,8,6,6,4,4,1,1,1 };//12 represents 2d6 and 8 represents 2d4
-        private static string[] Monster = new string[] { "DireRat","FireBeetle","HumanSkeleton","GiantCentipede","SpiderSwarm","HumanZombie","Choker","SkeletalChampion","Ghouls","GiantSpider","Cockatrice","GelatinousCube","RustMonster","Shadow","Wight","Stirge","Darkmantle","Troglodyte","Bugbear","Vargouilles","GrayOoze","Mimic","Ogre"};
+        private static int[] rollTable = new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14, 15, 15, 16, 16, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 23, 23 };
+        private static int[] ChallengeRating = new int[] { 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4 };
+        private static int[] EnemyNumbers = new int[] { 6, 6, 6, 4, 1, 6, 1, 1, 12, 4, 4, 1, 1, 1, 1, 1, 8, 6, 6, 4, 4, 1, 1, 1 };//12 represents 2d6 and 8 represents 2d4
+        private static List<string> Monster = new List<string> { "DireRat", "FireBeetle", "HumanSkeleton", "GiantCentipede", "SpiderSwarm", "HumanZombie", "Choker", "SkeletalChampion", "Ghouls", "GiantSpider", "Cockatrice", "GelatinousCube", "RustMonster", "Shadow", "Wight", "Stirge", "Darkmantle", "Troglodyte", "Bugbear", "Vargouilles", "GrayOoze", "Mimic", "Ogre" };
         private static D100 percentile = D100.GetInstance();
         private MonsterFactory monsterFactory = new MonsterFactory();
         private int roll;
@@ -44,14 +44,7 @@ namespace CSCD454_FinalProject.EncounterGeneration
 
         public int GetCR(string name)
         {
-            for (int i = 0; i < Monster.Length; i++)
-            {
-                if (name.Equals(name, StringComparison.OrdinalIgnoreCase))
-                {
-                    return ChallengeRating[i];
-                }
-            }
-            return 0;
+            return ChallengeRating[Monster.IndexOf(name)];
         }
 
         private int numberHelper(int n) //All "MAGIC" numbers represent number of potential monsters in encounter
---
+++ CSCD454-FinalProject/Maze/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSCD454_FinalProject.Combat;
+
+namespace CSCD454_FinalProject.Maze
+{
+    class Room
+    {
+        public Encounter encounter
+        {
+            get;
+            protected set
+            {
+                encounter = value;
+            }
+        }
+
+        public Boolean hasEncounter
+        {
+            get;
+            protected set;
+        }
+    }
+}
