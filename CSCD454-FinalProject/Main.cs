dc82e34f03865af477ac6b41ff517a02e6b78143
Stefan Bostain <zilulil@gmail.com>
2014-08-13 20:29:47 -0700

    Changed project to Console application

---
+++ CSCD454-FinalProject/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCD454_FinalProject
+{
+    public class TextUI
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
